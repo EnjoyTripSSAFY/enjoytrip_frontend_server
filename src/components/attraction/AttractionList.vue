1abec16c529c5d3786dcf76c1db2e25b960c826a
sample <sample@naver.com>
2023-11-15 14:19:49 +0900

    컴포넌트 파일 업로드

---
+++ src/components/attraction/AttractionList.vue
@@ -0,0 +1,20 @@
+<script setup>
+import KakaoMap from '@/components/attraction/item/KakaoMap.vue'
+import SelectType from "@/components/attraction/item/SelectType.vue";
+
+
+</script>
+<template>
+  <main
+    class="d-flex justify-content-center align-items-center my-auto"
+    style="padding: 10vh 0 5vh 0"
+  >
+    <div>
+      <h2 class="text-center">지역별 관광명소</h2>
+      <hr />
+      <SelectType/>
+      <KakaoMap />
+    </div>
+  </main>
+</template>
+
