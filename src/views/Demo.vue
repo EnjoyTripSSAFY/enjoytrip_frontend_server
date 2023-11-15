d3b893d7d7a8cfab4711e0e1322ed58b8237b0e3
sample <sample@naver.com>
2023-11-15 16:48:11 +0900

    헤더 및 푸터 구현, 로그인 및 회원가입 페이지 외면구현

---
+++ src/views/Demo.vue
@@ -0,0 +1,15 @@
+<template>
+  <a-layout style="min-height: 100vh">
+    <a-layout>
+      <CustomHeader/>
+      <a-layout-content style="margin: 0 16px">
+          <router-view></router-view>
+      </a-layout-content>
+      <CustomFooter/>
+    </a-layout>
+  </a-layout>
+</template>
+<script setup>
+import CustomHeader from "@/views/CustomHeader.vue";
+import CustomFooter from "@/views/CustomFooter.vue";
+</script>
