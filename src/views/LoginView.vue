4df085964e2c67db557e3cd0456c7e0c1f7f33eb
이상학 <yee950419@naver.com>
2023-11-20 17:50:09 +0900

    login 붙이는 중

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -8,18 +8,18 @@
     </div>
     <div class="login-container">
       <a-form
-        :model="loginUser"
+        :model="formState"
         name="normal_login"
         class="login-form"
         @finish="onFinish"
         @finishFailed="onFinishFailed"
       >
         <a-form-item
-          label="UserId"
+          label="Username"
           name="userId"
-          :rules="[{ required: true, message: 'Please input your Id!' }]"
+          :rules="[{ required: true, message: 'Please input your username!' }]"
         >
-          <a-input v-model:value="loginUser.userId">
+          <a-input v-model:value="formState.userId">
             <template #prefix>
               <UserOutlined class="site-form-item-icon" />
             </template>
@@ -28,10 +28,10 @@
 
         <a-form-item
           label="Password"
-          name="userPwd"
+          name="userPassword"
           :rules="[{ required: true, message: 'Please input your password!' }]"
         >
-          <a-input-password v-model:value="loginUser.userPwd">
+          <a-input-password v-model:value="formState.userPassword">
             <template #prefix>
               <LockOutlined class="site-form-item-icon" />
             </template>
@@ -40,13 +40,18 @@
 
         <a-form-item>
           <a-form-item name="remember" no-style>
-            <a-checkbox v-model:checked="loginUser.remember">Remember me</a-checkbox>
+            <a-checkbox v-model:checked="formState.remember">Remember me</a-checkbox>
           </a-form-item>
           <a class="login-form-forgot" href="">Forgot password</a>
         </a-form-item>
 
         <a-form-item>
-          <a-button :disabled="disabled" type="primary" class="login-form-button" @click="login">
+          <a-button
+            :disabled="disabled"
+            type="primary"
+            html-type="submit"
+            class="login-form-button"
+          >
             Log in
           </a-button>
           Or
@@ -77,41 +82,42 @@ const { changeMenuState } = useMenuStore()
 //   remember: true
 // })
 
-const userId = ref('')
-const userPwd = ref('')
-const remember = ref(true)
-
-const loginUser = reactive({
-  userId: userId.value,
-  userPwd: userPwd.value,
-  remember: remember.value
+const formState = reactive({
+  userId: '',
+  userPassword: '',
+  remember: true
 })
+// const onFinish = (values) => {
+//   console.log('Success:', values)
+// }
 
-const login = async () => {
-  console.log('data : ' + loginUser.value)
-  await userLogin(loginUser.value)
+const onFinish = async (values) => {
+  await userLogin(values)
   let token = sessionStorage.getItem('access-token')
+  console.log('isLogin : ' + isLogin)
   if (isLogin) {
     getUserInfo(token)
     changeMenuState()
   }
-  router.push('/')
+  router.push({ name: 'home' })
 }
 
-// const formState = reactive({
-//   username: '',
-//   password: '',
-//   remember: true
-// })
+// const login = async () => {
+//   console.log('data : ' + loginUser.value)
+//   await userLogin(loginUser.value)
+//   let token = sessionStorage.getItem('access-token')
+//   if (isLogin) {
+//     getUserInfo(token)
+//     changeMenuState()
+//   }
+//   router.push('/')
+// }
 
-const onFinish = (values) => {
-  console.log('Success:', values)
-}
 const onFinishFailed = (errorInfo) => {
   console.log('Failed:', errorInfo)
 }
 const disabled = computed(() => {
-  return !(loginUser.userId && loginUser.userPwd)
+  return !(formState.userId && formState.userPassword)
 })
 </script>
 
@@ -123,21 +129,18 @@ const disabled = computed(() => {
   justify-content: center;
   height: 70vh; /* 화면 전체 높이에 따라 중앙에 배치 */
 }
-
 .logo-container {
   display: flex;
   flex-direction: row;
   justify-content: center;
 }
-
 .login-container {
   display: flex;
   flex-direction: row-reverse;
   align-items: center;
   justify-content: center;
-  /* height: 70vh; 화면 전체 높이에 따라 중앙에 배치 */
+  //height: 70vh; /* 화면 전체 높이에 따라 중앙에 배치 */
 }
-
 #components-form-demo-normal-login .login-form {
   max-width: 300px;
 }
@@ -147,4 +150,5 @@ const disabled = computed(() => {
 #components-form-demo-normal-login .login-form-button {
   width: 100%;
 }
+/* 필요한 스타일을 추가하세요. */
 </style>
